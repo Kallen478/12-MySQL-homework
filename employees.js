0f44505ad0b39fab289c55812a48e2db6adf6fe2
Kyle Allen <Kallen478@gmail.com>
2020-11-17 16:36:27 -0500

    added functions for view employees by dept, view employees by role, and view all employees

--- employees.js
+++ employees.js
@@ -2,20 +2,20 @@ const inquirer = require("inquirer");
 const mysql = require("mysql");
 const consoleTable = require("console.table");
 const util = require("util");
-const AddEntriesFromIterable = require("es-abstract/2019/AddEntriesFromIterable");
+const { exit } = require("process");
 
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "rootroot",
-    database: "employees_DB"
-  });
+    database: "employees_db"
+});
 
 const conectionQuery = util.promisify(connection.query.bind(connection));
-
+// Main menu
 function mainMenu() {
-    inquirer.prompt ({
+    inquirer.prompt({
         name: "action",
         type: "list",
         message: "Main Menu",
@@ -29,37 +29,111 @@ function mainMenu() {
             "Update employee role"
         ]
     })
-    .then ((answer) => {
-        switch (answer.action) {
-            case "Add department":
-                addDept();
-            
-            case "Add role":
-                addRole();
+        .then((answer) => {
+            switch (answer.action) {
+                case "Add department":
+                    addDept();
 
-            case "Add employee":
-                addEmployee();
+                case "Add role":
+                    addRole();
 
-            case "View employees by department":
-                viewEmpByDept();
+                case "Add employee":
+                    addEmployee();
 
-            case "View employees by role":
-                viewEmpByRole();
+                case "View employees by department":
+                    viewEmpByDept();
 
-            case "View all employees":
-                viewAllEmp();
+                case "View employees by role":
+                    viewEmpByRole();
 
-            case "Update employee role":
-                updateEmpRole();
-        }
-    })
+                case "View all employees":
+                    viewAllEmp();
+
+                case "Update employee role":
+                    updateEmpRole();
+            }
+        })
 
 }
+// Add department 
 
- 
 
-connection.connect((err) => {
-    if (err) throw err;
-    console.log("Employee Tracker Main Menu");
-    mainMenu();
+// Add employee
+
+
+// View employees by department
+function viewEmpByDept() {
+    let deptList = [];
+    connectionQuery("SELECT name FROM department")
+        .then(value => {
+            for (let i = 0; i < value.length; i++) {
+                deptList.push(value[i].name)
+            };
+        })
+
+        .then(() => {
+            inquirer.prompt({
+                name: "department",
+                type: "list",
+                message: "Select a department to view",
+                choices: deptList
+            })
+
+                .then((answer) => {
+                    const query = `SELECT employee.id, employee.first_name, employee.last_name, role.title FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id WHERE department.name = "${answer.department}"`;
+                    connectionQuery(query).then(res => {
+                        console.log("\n");
+                        console.table(res);
+                        mainMenu();
+                    })
+                })
+        })
+}
+
+// View employees by role
+function viewEmpByRole() {
+    let roleList = [];
+    promisemysql.createConnection(connectionProperties)
+        .then((connection) => {
+            return connecton.query("SELECT title FROM role");
+        })
+        .then((roles) => {
+            for (i = 0; i < roles.length; i++) {
+                roleList.push(roles[i].title);
+            }
+        })
+        .then(() => {
+            inquirer.prompt({
+                name: "role",
+                type: "list",
+                message: "Select a role to search",
+                choices: roleList
+            })
+                .then((answer) => {
+                    const query = connectionQuery(query)
+                    .then(res => {
+                        console.log("\n");
+                        console.table(res);
+                        mainMenu();
+                })
+        })
+},
+// View all employees
+function viewAllEmp() {
+    const query = "SELECT * FROM employee";
+    connectionQuery(query)
+        .then(res => {
+            console.log("\n");
+            console.table(res);
+            mainMenu();
+        })
 });
+
+// Update employee role
+
+
+connection.connect(function (err) {
+        if (err) throw err;
+        console.log("Employee Tracker Main Menu");
+        mainMenu();
+})}
