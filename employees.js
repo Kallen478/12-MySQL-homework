208c17e7551b02c390e7aae4c93e17feaaca6c57
Kyle Allen <Kallen478@gmail.com>
2020-11-18 01:12:25 -0500

    update employee role function

--- employees.js
+++ employees.js
@@ -51,7 +51,7 @@ function mainMenu() {
 
                 case "Update employee role":
                     updateEmpRole();
-            }
+            };
         })
 }
 
@@ -77,37 +77,37 @@ function addRole() {
     connectionQuery("SELECT * from department")
         .then(res => {
             inquirer.prompt([
-                    {
-                        name: "role",
-                        type: "input",
-                        message: "Enter new role name",
-                    },
-                    {
-                        name: "salary",
-                        type: "input",
-                        message: "Enter salary amount",
-                    },
-                    {
-                        name: "department",
-                        type: "list",
-                        message: "Select the department the new role will be in",
-                        choices: () => {
-                            deptList = [];
-                            res.forEach(res => {
-                                deptList.push(res.name);
-                            });
-                            return deptList;
-                        }
+                {
+                    name: "role",
+                    type: "input",
+                    message: "Enter new role name",
+                },
+                {
+                    name: "salary",
+                    type: "input",
+                    message: "Enter salary amount",
+                },
+                {
+                    name: "department",
+                    type: "list",
+                    message: "Select the department the new role will be in",
+                    choices: () => {
+                        deptList = [];
+                        res.forEach(res => {
+                            deptList.push(res.name);
+                        });
+                        return deptList;
                     }
-                ])
+                }
+            ])
                 .then((answer) => {
-                        let deptChoice = res.filter((res) => res.name == answer.department);
-                        connection.query("INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)", [answer.role, answer.salary, deptChoice[0].id],
-                            (err) => {
-                                if (err) throw err;
-                                console.log("Role sucessfully added.");
-                            });
-                        mainMenu();
+                    let deptChoice = res.filter((res) => res.name == answer.department);
+                    connection.query("INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)", [answer.role, answer.salary, deptChoice[0].id],
+                        (err) => {
+                            if (err) throw err;
+                            console.log("Role sucessfully added.");
+                        });
+                    mainMenu();
                 })
         })
 }
@@ -117,75 +117,73 @@ function addEmployee() {
     connectionQuery("SELECT * from role")
         .then(res => {
             inquirer.prompt([{
-                    name: "firstName",
-                    type: "input",
-                    message: "Enter first name",
-                },
-                {
-                    name: "lastName",
-                    type: "input",
-                    message: "Enter last name",
-                },
-                {
-                    name: "roleName",
-                    type: "list",
-                    message: "Select role for the new employee",
-                    choices: () => {
-                        rolesList = [];
-                        res.forEach(result => {
-                            rolesList.push(result.title);
-                        });
-                        return rolesList;
-                    }
+                name: "firstName",
+                type: "input",
+                message: "Enter first name",
+            },
+            {
+                name: "lastName",
+                type: "input",
+                message: "Enter last name",
+            },
+            {
+                name: "roleName",
+                type: "list",
+                message: "Select role for the new employee",
+                choices: () => {
+                    rolesList = [];
+                    res.forEach(res => {
+                        rolesList.push(res.title);
+                    });
+                    return rolesList;
                 }
-                ])
+            }
+            ])
                 .then((answer) => {
-                        connection.query('SELECT * FROM role', (err, res) => {
-                                if (err)
-                                    throw (err);
-                                let roleChoice = res.filter((res) => res.title == answer.roleName);
+                    connection.query('SELECT * FROM role', (err, res) => {
+                        if (err)
+                            throw (err);
+                        let roleChoice = res.filter((res) => res.title == answer.roleName);
 
-                                connection.query("SELECT * FROM employee", (res) => {
-                                        inquirer.prompt([
-                                            {
-                                                name: "manager",
-                                                type: "list",
-                                                message: "Select manager for the new employee",
-                                                choices: () => {
-                                                    managersList = [];
-                                                    res.forEach(res => {
-                                                        managersList.push(res.last_name);
-                                                    });
-                                                    managersList.push("No manager");
-                                                    return managersList;
-                                                }
-                                            }
-                                        ])
-                                            .then((managerAnswer) => {
-                                                    connection.query("SELECT * FROM employee", (err, res) => {
-                                                            if (err)
-                                                                throw (err);
-                                                            let managerChoice = res.filter((res) => res.last_name == managerAnswer.manager);
+                        connection.query("SELECT * FROM employee", (res) => {
+                            inquirer.prompt([
+                                {
+                                    name: "manager",
+                                    type: "list",
+                                    message: "Select manager for the new employee",
+                                    choices: () => {
+                                        managersList = [];
+                                        res.forEach(res => {
+                                            managersList.push(res.last_name);
+                                        });
+                                        managersList.push("No manager");
+                                        return managersList;
+                                    }
+                                }
+                            ])
+                                .then((managerAnswer) => {
+                                    connection.query("SELECT * FROM employee", (err, res) => {
+                                        if (err) throw (err);
+                                        let managerChoice = res.filter((res) => res.last_name == managerAnswer.manager);
 
-                                                            if (managerAnswer.manager === "No manager") {
-                                                                managerChoice[0].id = null;
-                                                            }
-                                                            else {
-                                                                managerChoice[0].id;
-                                                            }
+                                        if (managerAnswer.manager === "No manager") {
+                                            managerChoiceID = null;
+                                        }
+                                        else {
+                                            managerChoiceID = managerChoice[0].id;
+                                        }
 
-                                                            connection.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", [answer.firstName, answer.lastName, roleChoice[0].id, managerChoice[0].id],
-                                                                (err) => {
-                                                                    if (err)
-                                                                        throw err;
-                                                                    console.log("Employee sucessfully added.");
-                                                                });
-                                                            mainMenu();
-                                                        });
-                                                });
+                                        connection.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", [answer.firstName, answer.lastName, roleChoice[0].id, managerChoiceID],
+                                            (err) => {
+                                                if (err) throw err;
+                                                console.log("Employee sucessfully added.");
+                                            });
+                                        mainMenu();
                                     });
-                            });
-                    })
+                                });
+                        });
+                    });
+                })
         })
 }
 
@@ -256,13 +254,89 @@ function viewAllEmp() {
             console.table(res);
             mainMenu();
         })
-});
+},
 
 // Update employee role
+function updateEmpRole() {
+    connection.query("SELECT * FROM employee", (res) => {
+            inquirer.prompt([
+                {
+                    name: "employee",
+                    type: "list",
+                    message: "Select employee to update role",
+                    choices: () => {
+                        employeeList = [];
+                        res.forEach(res => {
+                            employeeList.push(res.last_name);
+                        });
+                        return employeeList;
+                    }
+                }
+            ])
+                .then((answer) => {
+                        let employeeChoice = res.filter((res) => res.last_name == answer.employee);
+                        connection.query("SELECT * FROM role", (res) => {
+                                inquirer.prompt([
+                                    {
+                                        name: "roleName",
+                                        type: "list",
+                                        message: "Select new role",
+                                        choices: () => {
+                                            rolesList = [];
+                                            res.forEach(res => {
+                                                rolesList.push(res.title);
+                                            });
+                                            return rolesList;
+                                        }
+                                    }
+                                ]).then((roleAnswer) => {
+                                        let roleChoice = res.filter((res) => res.title == roleAnswer.roleName);
+                                        connection.query("SELECT * FROM employee", (res) => {
+                                                inquirer.prompt([
+                                                    {
+                                                        name: "manager",
+                                                        type: "list",
+                                                        message: "Select new manager",
+                                                        choices: () => {
+                                                            managersList = [];
+                                                            res.forEach(res => {
+                                                                managersList.push(res.last_name);
+                                                            });
+                                                            managersList.push("No manager");
+                                                            return managersList;
+                                                        }
+                                                    }
+                                                ]).then((managerAnswer) => {
+                                                    connection.query("SELECT * FROM employee", (err, res) => {
+                                                        if (err)
+                                                            throw (err);
+                                                        let managerChoice = res.filter((res) => res.last_name == managerAnswer.manager);
 
+                                                        if (managerAnswer.manager === "No manager") {
+                                                            managerChoiceID = null;
+                                                        }
+                                                        else {
+                                                            managerChoiceID = managerChoice[0].id;
+                                                        }
+
+                                                        connection.query("UPDATE employee SET role_id = ?, manager_id = ? WHERE id = ?", [roleChoice[0].id, managerChoiceID, employeeChoice[0].id], 
+                                                        (err) => {
+                                                                if (err) throw err;
+                                                                console.log("Employee role sucessfully updated");
+                                                            });
+                                                        mainMenu();
+                                                    });
+                                                });
+                                            });
+                                    });
+                            });
+                    });
+        })
+})
 
 connection.connect((err) => {
-        if (err) throw err;
-        console.log("Employee Tracker Main Menu");
-        mainMenu();
-    })}
+    if (err) throw err;
+    console.log("Employee Tracker Main Menu");
+    mainMenu();
+})
+}
